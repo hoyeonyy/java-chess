89db0b1c88533705dc3c7fca1745e85d73cc055e
hoyeonyy <highoption@naver.com>
2024-04-01 13:31:09 +0900

    feat (Init.sql) : 테이블 설계도

---
+++ docker/db/mysql/init/init.sql
@@ -0,0 +1,16 @@
+USE chess;
+
+CREATE TABLE chess_game
+(
+    id    bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state varchar(12)
+);
+
+create table piece
+(
+    id     bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    file   char(1),
+    `rank` int,
+    type   varchar(10),
+    color  varchar(5)
+);
