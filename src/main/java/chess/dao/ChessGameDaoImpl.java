291e0897b999d504e35db59e502e058c0163262b
hoyeonyy <highoption@naver.com>
2024-04-03 17:56:28 +0900

    feat (DbConnection) : 커넥션 연결 객체 생성

--- src/main/java/chess/dao/ChessGameDaoImpl.java
+++ src/main/java/chess/dao/ChessGameDaoImpl.java
@@ -9,39 +9,21 @@ import chess.domain.game.WhiteTurn;
 import chess.domain.piece.Piece;
 import chess.domain.position.Position;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
 
 public class ChessGameDaoImpl implements ChessGameDao {
-    private static final String SERVER = "localhost:13306"; // MySQL 서버 주소
-    private static final String DATABASE = "chess"; // MySQL DATABASE 이름
-    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "root"; //  MySQL 서버 아이디
-    private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
-
     private final PieceDao pieceDao;
 
     public ChessGameDaoImpl(PieceDao pieceDao) {
         this.pieceDao = pieceDao;
     }
 
-    public Connection getConnection() {
-        // 드라이버 연결
-        try {
-            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
-        } catch (final SQLException e) {
-            System.err.println("DB 연결 오류:" + e.getMessage());
-            e.printStackTrace();
-            return null;
-        }
-    }
-
     @Override
     public void createChessGame(GameState gameState) {
-        try (Connection connection = getConnection();
+        try (Connection connection = DbConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(
                      "INSERT INTO chess_game (state) VALUES (?)")) {
             statement.setString(1, gameState.getStateName());
@@ -55,7 +37,7 @@ public class ChessGameDaoImpl implements ChessGameDao {
 
     @Override
     public GameState findGame() {
-        try (Connection connection = getConnection();
+        try (Connection connection = DbConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(
                      "SELECT state FROM chess_game")) {
             return retrieveChessGameFrom(statement.executeQuery());
@@ -85,7 +67,7 @@ public class ChessGameDaoImpl implements ChessGameDao {
 
     @Override
     public void removeGame() {
-        try (Connection connection = getConnection();
+        try (Connection connection = DbConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(
                      "DELETE FROM chess_game")) {
             pieceDao.removePieces();
--- src/main/java/chess/dao/PieceDaoImpl.java
+++ src/main/java/chess/dao/PieceDaoImpl.java
@@ -5,7 +5,6 @@ import chess.domain.position.File;
 import chess.domain.position.Position;
 import chess.domain.position.Rank;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,26 +12,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PieceDaoImpl implements PieceDao {
-    private static final String SERVER = "localhost:13306"; // MySQL 서버 주소
-    private static final String DATABASE = "chess"; // MySQL DATABASE 이름
-    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "root"; //  MySQL 서버 아이디
-    private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
-
-    public Connection getConnection() {
-        // 드라이버 연결
-        try {
-            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
-        } catch (final SQLException e) {
-            System.err.println("DB 연결 오류:" + e.getMessage());
-            e.printStackTrace();
-            return null;
-        }
-    }
 
     @Override
     public void saveAllPieces(Map<Position, Piece> pieces) {
-        try (Connection connection = getConnection();
+        try (Connection connection = DbConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(
                      "INSERT INTO piece (file, `rank`, type, color) VALUES (?, ?, ?, ?)")) {
             removePieces();
@@ -53,7 +36,7 @@ public class PieceDaoImpl implements PieceDao {
 
     @Override
     public Map<Position, Piece> findPieces() {
-        try (Connection connection = getConnection();
+        try (Connection connection = DbConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(
                      "SELECT file, `rank`, type, color FROM piece")) {
             ResultSet resultSet = statement.executeQuery();
@@ -77,7 +60,7 @@ public class PieceDaoImpl implements PieceDao {
 
     @Override
     public void removePieces() {
-        try (Connection connection = getConnection();
+        try (Connection connection = DbConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(
                      "DELETE FROM piece")) {
             statement.executeUpdate();
